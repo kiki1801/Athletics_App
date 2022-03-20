5e117a6b13c6c3da3503a95a5e6b8f820e901e7a
kyllianj <98753607+kyllianj@users.noreply.github.com>
2022-03-20 12:23:14 +0100

    Add files via upload

---
+++ function_all.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 11:33:18 2022
+
+@author: kyllian
+"""
+
+#Function to make choice while running the code
+def choice(list_of_choice):
+    i = 1
+    for element in list_of_choice:
+        print('{} : {}'.format(str(i),element))
+        i = i+1
+        
+#A - Create links for IAAF calendar pages        
+#Create links for IAAF calendar pages
+def create_calendar_links():
+    calendar_urls = ['https://www.worldathletics.org/competition/calendar-results?']
+    for i in range(100,15*100,100):
+        things_make_link = ['https://www.worldathletics.org/competition/calendar-results?offset=',str(i)]    
+        calendar_url = ''.join(things_make_link)
+        calendar_urls.append(calendar_url) 
+        
+    return calendar_urls
+
+###############################################################################
+
+#B - Get informations of each competition on each calendar's page
+#Library
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import urllib3
+import json 
+
+#Function to scrap 
+def get_content(regex,url):
+    req = urllib3.PoolManager()
+    res = req.request('GET',url)
+    soup = BeautifulSoup(res.data,'html.parser')
+    content = soup.find_all(regex)
+    
+    return content
+    
+def get_competitions_infos(list_with_urls):
+    empty_df = pd.DataFrame()
+    for link in list_with_urls:
+        content = get_content('script',link)
+        content = content[1]
+        
+        json_object = content.text
+        get_dict = json.loads(json_object)
+        infos_dict = get_dict["props"]["pageProps"]["initialEvents"]['results']
+        
+        df = pd.DataFrame(infos_dict)
+        empty_df = pd.concat([empty_df,df])
+        
+        empty_df = empty_df.reset_index(drop=True)
+    
+    return empty_df
+
+###############################################################################
+
+#C - Keep only competitions we need
+#Library 
+import datetime
+
+#Keep the ones between today and n past weeks
+def filter_df(dataframe,n):
+    #Keep the ones with results
+    dataframe_bis = dataframe.loc[(dataframe['hasResults'] == True )]
+        
+    #Delete row with NA in column disciplines -> Race Walking 
+    dataframe_bis = dataframe_bis[dataframe_bis['disciplines'].notna()]
+
+    #Keep Stadium Indoor & Outdoor
+    dataframe_bis = dataframe_bis[dataframe_bis['disciplines'].str.contains('Stadium Indoor|Stadium Outdoor')]
+    
+    today = datetime.date.today()
+    date_n_weeks_before = today - datetime.timedelta(weeks=n)
+    
+    dataframe_bis['endDate'] = pd.to_datetime(dataframe_bis['endDate']).dt.date
+    filtered_df = dataframe_bis[(dataframe_bis['endDate'] >= date_n_weeks_before)]
+    
+    filtered_df = filtered_df.reset_index(drop=True)
+    
+    return filtered_df
+
+
+
+
---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 11:31:02 2022
+
+@author: kyllian
+"""
+
+#0 - Library
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import urllib3
+import json 
+import datetime
+
+#0 - Import function form other file
+from function_all import choice,create_calendar_links,get_competitions_infos,filter_df
+
+#1 - Choose your language
+language_list = ['English','French']
+
+choice(language_list)
+language = str(input('Choose your language : '))
+
+#PROGRAM IN ENGLISH OR IN FRENCH
+if language == 1:
+    print('Coming Soon...')
+    #A - Create links for IAAF calendar pages
+    calendar_urls = create_calendar_links()
+    
+    #B - Get information of each competition on each calendar's page
+    competition_infos_df = get_competitions_infos(calendar_urls)
+    
+    #C - Keep only competitions we need
+    number_of_weeks = int(input('How many past weeks you want to keep ? : '))
+else:
+    print('Prochainement...')
+    #A - Créer les liens pour les pages du calendrier IAAF
+    calendar_urls = create_calendar_links()
+    
+    #B - Scrap les informations de chaque compétition de chaque page du calendrier
+    competition_infos_df = get_competitions_infos(calendar_urls)
+    
+    #C - Filtrer le tableau des compétitions
+    number_of_weeks = int(input('Combien de semaines passées vous voulez garder ? : '))
+    filtered_df = filter_df(competition_infos_df,number_of_weeks)
+
+
+
+
+
