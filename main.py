e538a427ba96292fbf5cacb169bb5edc04dfc979
kyllianj <98753607+kyllianj@users.noreply.github.com>
2022-03-22 21:10:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,28 +25,153 @@ language = int(input('Choose your language : '))
 
 #PROGRAM IN ENGLISH OR IN FRENCH
 if language == 1:
-    print('Coming Soon...')
-    #A - Create links for IAAF calendar pages
+    #1A - Create links for IAAF calendar pages
     calendar_urls = create_calendar_links()
     
-    #B - Get information of each competition on each calendar's page
+    #1B - Get information of each competition on each calendar's page
     competition_infos_df = get_competitions_infos(calendar_urls)
     
-    #C - Keep only competitions we need
+    #1C - Keep only competitions we need
     number_of_weeks = int(input('How many past weeks you want to keep ? : '))
+    filtered_df = filter_df(competition_infos_df,number_of_weeks)
+    
+    #2 - Choose between 3 features
+    features_list = ['Look at the Top N Marks','Any Records ?','Winning Probability ?']
+    choice(features_list)
+    feature = int(input('Select one feature : '))
+    
+    #2A - Look at the Top N Marks
+    if feature == 1:
+        #Tell yes if you want to check for a specific nationality
+        yes_no_list = ['Yes','No']
+        choice(yes_no_list)
+        yes_no = int(input('Do you want to look at the top N marks for a specific nationality ? '))
+        
+        if yes_no == 1:
+            print('Coming Soon...')
+            #Filter for choosen nationality and then run the same code as if you don't want to look for a specific nationality
+        else:
+            number_of_marks = int(input('Top N Marks ? Choose N : '))
+            
+            #Choose between Men and Women
+            genders_list = ['Men','Women']
+            choice(genders_list) 
+            gender = int(input('Select one : '))
+            
+            if gender == 1:
+                #Choose between type of event
+                event_types_list = ['Races','Jumps','Throws']
+                choice(event_types_list)
+                event_type = int(input('Which kind of events ? '))
+                
+                if event_type == 1:
+                    #Choose a race event
+                    races_list = ["60m","100m","200m","400m","60mH","110mH","400mH","800m","1500m","3000m","3000mSC","5000m","10000m"]
+                    choice(races_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+                elif event_type == 2:
+                    #Choose a jump event
+                    jumps_list = ["Long Jump","Triple Jump","High Jump","Pole Vault"]
+                    choice(jumps_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+                else:
+                    #Choose a throw event
+                    throws_list = ["Shot Put","Discus Throw","Hammer Throw","Javelin Throw"]
+                    choice(throws_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+            else:
+                #Choose between type of event
+                event_types_list = ['Races','Jumps','Throws']
+                choice(event_types_list)
+                event_type = int(input('Which kind of events ? '))
+                
+                if event_type == 1:
+                    #Choose a race event
+                    races_list = ["60m","100m","200m","400m","60mH","100mH","400mH","800m","1500m","3000m","3000mSC","5000m","10000m"]
+                    choice(races_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+                elif event_type == 2:
+                    #Choose a jump event
+                    jumps_list = ["Long Jump","Triple Jump","High Jump","Pole Vault"]
+                    choice(jumps_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+                else:
+                    #Choose a throw event
+                    throws_list = ["Shot Put","Discus Throw","Hammer Throw","Javelin Throw"]
+                    choice(throws_list)
+                    race = int(input('Select one : '))
+                    print('Coming Soon...')
+    #2B -  Any Records ?
+    elif feature == 2:
+        print('Coming Soon...')
+    #2C - Winning Probability ?
+    else:
+        #Choose between Men and Women
+        genders_list = ['Men','Women']
+        choice(genders_list) 
+        gender = int(input('Select one : '))
+        
+        if gender == 1:
+            #Choose between type of event
+            event_types_list = ['Races','Jumps','Throws']
+            choice(event_types_list)
+            event_type = int(input('Which kind of events ? '))
+            
+            if event_type == 1:
+                #Choose a race event
+                races_list = ["60m","100m","200m","400m","60mH","110mH","400mH","800m","1500m","3000m","3000mSC","5000m","10000m"]
+                choice(races_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
+            elif event_type == 2:
+                #Choose a jump event
+                jumps_list = ["Long Jump","Triple Jump","High Jump","Pole Vault"]
+                choice(jumps_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
+            else:
+                #Choose a throw event
+                throws_list = ["Shot Put","Discus Throw","Hammer Throw","Javelin Throw"]
+                choice(throws_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
+        else:
+            #Choose between type of event
+            event_types_list = ['Races','Jumps','Throws']
+            choice(event_types_list)
+            event_type = int(input('Which kind of events ? '))
+            
+            if event_type == 1:
+                #Choose a race event
+                races_list = ["60m","100m","200m","400m","60mH","100mH","400mH","800m","1500m","3000m","3000mSC","5000m","10000m"]
+                choice(races_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
+            elif event_type == 2:
+                #Choose a jump event
+                jumps_list = ["Long Jump","Triple Jump","High Jump","Pole Vault"]
+                choice(jumps_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
+            else:
+                #Choose a throw event
+                throws_list = ["Shot Put","Discus Throw","Hammer Throw","Javelin Throw"]
+                choice(throws_list)
+                race = int(input('Select one : '))
+                print('Coming Soon...')
 else:
     print('Prochainement...')
-    #A - Créer les liens pour les pages du calendrier IAAF
+    #1A - Créer les liens pour les pages du calendrier IAAF
     calendar_urls = create_calendar_links()
     
-    #B - Scrap les informations de chaque compétition de chaque page du calendrier
+    #1B - Scrap les informations de chaque compétition de chaque page du calendrier
     competition_infos_df = get_competitions_infos(calendar_urls)
     
-    #C - Filtrer le tableau des compétitions
+    #1C - Filtrer le tableau des compétitions
     number_of_weeks = int(input('Combien de semaines passées vous voulez garder ? : '))
     filtered_df = filter_df(competition_infos_df,number_of_weeks)
-
-
-
-
-
